2d1c8b3f85bcc814063d66291afc6cf577dbd752
Seulgi Gwak <noel25vibe@gmail.com>
2024-02-20 21:52:01 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const API_KEY = `cd07bb6bc3654d55bea35f015d942fbb`
+let news = []
+
+const getLatesNews = async()=>{
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    news = data.articles
+    console.log("ddd", news)
+}
+ 
+getLatesNews()
