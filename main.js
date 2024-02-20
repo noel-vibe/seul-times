5207ab70f54898f6071ca0dbc52caa694d876851
Seulgi Gwak <noel25vibe@gmail.com>
2024-02-20 22:10:00 +0900

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `cd07bb6bc3654d55bea35f015d942fbb`
 let news = []
 
 const getLatesNews = async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url = new URL(`https://seul-times.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`)
     const response = await fetch(url)
     const data = await response.json()
     news = data.articles
